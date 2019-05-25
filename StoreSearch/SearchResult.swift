6c66280085e03ead58401142ebed9379d0246d38
Brett Wallace <bwallace90@gmail.com>
2019-05-25 13:15:52 -0400

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Brett Wallace on 5/25/19.
+//  Copyright © 2019 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
