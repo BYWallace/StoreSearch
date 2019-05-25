9f638b31f1246fc2b8a04bd2989259053f9526b4
Brett Wallace <bwallace90@gmail.com>
2019-05-25 11:59:41 -0400

    Initial commit and rename ViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by Brett Wallace on 5/25/19.
+//  Copyright © 2019 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class
+roller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
