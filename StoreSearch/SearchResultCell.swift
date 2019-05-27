b5acb6d9cb9c1f4a296149151d9bb1435c23a0ed
Brett Wallace <bwallace90@gmail.com>
2019-05-27 11:27:18 -0400

    Use custom cells for search results.

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Brett Wallace on 5/27/19.
+//  Copyright © 2019 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
